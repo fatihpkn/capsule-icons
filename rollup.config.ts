529bbda51fbe45ae178e5e9a7a3ea4d9b56413ca
fatihpiskin <fatih.piskin@enuygun.com>
2022-02-28 17:48:13 +0300

    update

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,15 +1,28 @@
 import { babel } from "@rollup/plugin-babel";
 import filesize from "rollup-plugin-filesize";
 import typescript from "rollup-plugin-typescript2";
+import tsTreeshaking from "rollup-plugin-ts-treeshaking";
+
+import resolve from '@rollup/plugin-node-resolve'
+
 
 const config = {
-  input: "src/icons/index.ts",
-  output: {
-    file: "dist/index.esm.js",
-    format: "esm",
-  },
+  input: './src/index.ts',
+  output: [
+    {
+      // Bundle into ESM for modern consumers.
+      // Only ESM build can currently be tree-shaken.
+      dir: 'dist/esm',
+      format: 'esm',
+    },
+    {
+      // Bundle into CJS for other consumers.
+      dir: 'dist/cjs',
+      format: 'cjs',
+    },
+  ],
   external: [/@babel\/runtime/, "react"],
-  plugins: [typescript({ useTsconfigDeclarationDir: true }), babel({ babelHelpers: "runtime", plugins: ["@babel/plugin-transform-runtime"] }), filesize()],
+  plugins: [typescript({ useTsconfigDeclarationDir: true }), tsTreeshaking({ js: true }), filesize()],
 };
 
 export default config;
